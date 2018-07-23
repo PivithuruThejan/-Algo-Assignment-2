6e90ebbe278859d78a8b25aa469b7c8b5444f654
PivithuruThejan <pivithuru.14@cse.mrt.ac.lk>
2018-07-23 15:27:19 +0530

    tests added

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,35 +1,100 @@
-import random
-def binarySearch (arr, s, e, x):
-	if e >= s:
-		mid = s + (e - s)/2# creates the mid point
-		if arr[mid] == x:
-			return mid
-		elif arr[mid] > x:
-			return binarySearch(arr, s, mid-1, x)
-		else:
-			return binarySearch(arr, mid+1, e, x)
-	else:
-		return -1
+from random import randint
+import time
 
 
-def randomizedbinarySearch (arr, s, e, x):
-	if e >= s:
-		point = random.randint(s, e)#creates a random point
-		if arr[point] == x:
-			return mid
-		elif arr[point] > x:
-			return binarySearch(arr, s, point-1, x)
-		else:
-			return binarySearch(arr, point+1, e, x)
-	else:
-		return -1
+def binarySearch(element, array, i, j):
+    if i <= j:
+        mid = (i + j) / 2
+        if array[mid] == element:#finds the mid point
+            return mid
+        else:
+            if element < array[mid]:
+                return binarySearch(element, array, i, mid - 1)
+            else:
+                return binarySearch(element, array, mid + 1, j)
+    else:
+        return -1
 
-arr = []
-for x in range(10000000):
-  arr.append(x)
-print(len(arr))
-x = -1
 
-result = binarySearch(arr, 0, len(arr)-1, x)
+def randomizedBinarySearch(element, array, i, j):
+    if i <= j:
+        randomIndex = randint(i, j)#finds a random point
+        if array[randomIndex] == element:
+            return randomIndex
+        else:
+            if element < array[randomIndex]:
+                return randomizedBinarySearch(element, array, i, randomIndex - 1)
+            else:
+                return randomizedBinarySearch(element, array, randomIndex + 1, j)
+    else:
+        return -1
 
-print(result)
+
+def arrayPrepare(size):
+    i = 0
+    n = 0
+    array = []
+    members = []
+    nonMembersTemperary = []
+    nonMembersCount = 0
+    membersCount = 0
+
+    memberRange = size / 700
+
+    while i < size:
+        if randint(0, 1):
+            array.append(n)
+            if membersCount < 700 and i % memberRange == 0:
+                members.append(n)
+                membersCount += 1
+            i += 1
+        else:
+            nonMembersTemperary.append(n)
+            nonMembersCount += 1
+        n += 1
+
+    while nonMembersCount < 300:
+        if randint(0, 1):
+            nonMembersTemperary.append(n)
+            nonMembersCount += 1
+        n += 1
+    nonMemberRange = len(nonMembersTemperary) / 300
+    nonMembers = [nonMembersTemperary[i * nonMemberRange] for i in range(300)]
+
+    return array, nonMembers, members
+
+
+for k in range(5, 8):
+
+    arraySize = 10 ** k
+
+    print("Test " + str(k - 4) + "-----------------------")
+    print("")
+
+    array, nonMembers, members = arrayPrepare(arraySize)
+
+    # Binary Search
+    start = time.time()
+    for m in members:
+        binarySearch(m, array, 0, arraySize - 1)
+
+    for nm in nonMembers:
+        binarySearch(nm, array, 0, arraySize - 1)
+    end = time.time()
+
+    print("Binary Search(N = " + str(arraySize) + ") - Time(microseconds) : " + str(
+        1000000 * (end - start) / 1000))
+    print("")
+
+    # Randomized Binary Search
+    start = time.time()
+    for m in members:
+        randomizedBinarySearch(m, array, 0, arraySize - 1)
+
+    for nm in nonMembers:
+        randomizedBinarySearch(nm, array, 0, arraySize - 1)
+    end = time.time()
+
+    print("Randomized Binary Search(N = " + str(arraySize) + ") - Time(microseconds) : " + str(
+        1000000 * (end - start) / 1000))
+    print("")
